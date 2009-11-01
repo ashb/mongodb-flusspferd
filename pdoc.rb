a6f1f5cf07c4ebf3bd36fa2623906f2c0cf27249
Ash Berlin <ash_github@firemirror.com>
2009-11-01 13:21:53 +0000

    Write some pdoc and a (simple generator script that wont work for anyone else :)

---
+++ pdoc.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'rake'
+require '../pdoc/lib/pdoc'
+
+rm_rf "docs"
+begin
+  files = Dir.glob("src/*.jsdoc")
+  files << {
+    :output => "docs",
+    :templates => "../prototype-pdoc-template/html",
+    :syntax_highlighter => :none
+  }
+  PDoc::Runner.new(*files).run
+rescue PDoc::ParseError => e
+  print e.message
+  #print "\n"
+  #print e.failures
+  print e.file
+  print "\n"
+
+rescue ArgumentError => e
+  print "ArgError\n"
+end
+
