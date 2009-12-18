b4723c7b38a791ddd27d2268d3126fae108aeea2
Ash Berlin <ash_github@firemirror.com>
2009-12-18 02:44:31 +0000

    Make pdoc script slightly more useful

--- pdoc.rb
+++ pdoc.rb
@@ -1,13 +1,13 @@
 require 'rubygems'
 require 'rake'
-require '../pdoc/lib/pdoc'
+require '../flusspferd/vendor/pdoc/lib/pdoc'
 
 rm_rf "docs"
 begin
-  files = Dir.glob("src/*.jsdoc")
+  files = Dir.glob("src/*.pdoc")
   files << {
     :output => "docs",
-    :templates => "../prototype-pdoc-template/html",
+    :templates => "../flusspferd/vendor/pdoc-template/html",
     :syntax_highlighter => :none,
     :index_page => 'README.markdown'
   }
